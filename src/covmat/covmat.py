407931f45047001b9bdad8807c3d166b24337263
RosalynLP <rosalynlaurap@gmail.com>
2020-10-20 15:12:23 +0100

    producing plots

--- src/covmat/covmat.py
+++ src/covmat/covmat.py
@@ -21,7 +21,7 @@ import sys
 
 np.set_printoptions(threshold=sys.maxsize)
 
-sns.set()
+sns.set_style("white")
 
 def matrix_plot_labels(df):
     explabels = [x[0] for x in df.index]
@@ -35,11 +35,11 @@ def matrix_plot_labels(df):
             unique_ds.append([labels[x + 1], x + 1])
     ticklabels = [unique_ds[x][0] for x in range(len(unique_ds))]
     # Renaming ticklabels
-    ticklabel_dict = {"NMCPD": "NMC",
-                      "NMCPD_D": "NMC",
-                      "SLACD": "SLAC",
-                      "BCDMSD": "BCDMS",
-                      "DYE886_D": "NuSea"}
+    ticklabel_dict = {"CHORUSNUPb": r"CHORUS $\nu$",
+                      "CHORUSNBPb": r"CHORUS $\bar{\nu}$",
+                      "NTVNUDMNFe": r"NuTeV $\nu$",
+                      "NTVNBDMNFe": r"NuTeV $\bar{\nu}$",
+                      "DYE605": "DYE605"}
     ticklabels = [ticklabel_dict[ticklabel] for ticklabel in ticklabels]
     startlocs = [unique_ds[x][1] for x in range(len(unique_ds))]
     startlocs += [len(labels)]
@@ -49,16 +49,16 @@ def matrix_plot_labels(df):
     return ticklocs, ticklabels, startlocs
 
 def corrmat_plot(matrix, labeldf, descrip, label):
-    fig, ax = plt.subplots(figsize=(7,7))
+    fig, ax = plt.subplots(figsize=(10,10))
     diag_minus_half = (np.diagonal(matrix))**(-0.5)
     corrmat = diag_minus_half[:,np.newaxis]*matrix*diag_minus_half
-    matrixplot = ax.matshow(corrmat,
+    matrixplot = ax.matshow(np.nan_to_num(corrmat),
                             cmap=cm.Spectral_r,
                             vmin = -1,
                             vmax = +1)
     cbar=fig.colorbar(matrixplot, fraction=0.046, pad=0.04)
     cbar.ax.tick_params(labelsize=12)
-  #  ax.set_title(f"{descrip} correlation matrix", fontsize=15)
+    ax.set_title(f"{descrip} correlation matrix", fontsize=25)
     ticklocs, ticklabels, startlocs = matrix_plot_labels(labeldf)
     plt.xticks(ticklocs, ticklabels, rotation=45, fontsize=15)
     plt.gca().xaxis.tick_bottom()
@@ -73,51 +73,19 @@ def corrmat_plot(matrix, labeldf, descrip, label):
     plt.savefig(f"../../plots/covmats/covmats_{descrip}_{label}.png")
     return fig
 
-def covmat_plots(label, fp1_table, fp2_table, fp1_covmat):
-
-    expcovmat = fp1_covmat.values
-
-    T_fp2 = fp2_table["theory_central"]
-    D = fp1_table["data_central"]
-    T_fp1 = fp1_table["theory_central"]
-    
-    # Calculating errors on T_fp1 by taking standard deviation
-    T_fp1_reps = fp1_table.loc[:, fp1_table.columns.str.contains("rep")]
-    nrep = len(T_fp1_reps.values.T)
-
-    T_fp2_repeat = np.tile(T_fp2.values, (nrep,1)).T
-    deltas = T_fp1_reps.values - T_fp2_repeat
-
-    T_fp1_repeat = np.tile(T_fp1.values, (nrep,1)).T
-    deltasprime =  T_fp1_reps.values - T_fp1_repeat
-
-    covmat = (1/nrep) * deltas@deltas.T
-    np.savetxt(f"covmatrix_{label}.csv", covmat, delimiter=",")
-    covmatprime = (1/nrep) * deltasprime@deltasprime.T
-    np.savetxt(f"covmatrix_shift_{label}.csv", covmatprime, delimiter=",")
-    
-    normcovmat = covmat/np.outer(D.values, D.values)
-    normexpcovmat = expcovmat/np.outer(D.values, D.values)
-    expsqrtdiags = np.sqrt(np.diag(normexpcovmat))
-    totcovmat = covmat + expcovmat
-    totnormcovmat = totcovmat/np.outer(D.values, D.values)
-    
-    fig_th = corrmat_plot(covmat, fp1_table, "theory", label)
-    fig_exp = corrmat_plot(expcovmat, fp1_table, "experiment", label)
-    fig_tot = corrmat_plot(totcovmat, fp1_table, "total", label)
-
+def diag_element_plot(normcovmat, totnormcovmat, expsqrtdiags, descrip, label):
     # Diag element plot
     sqrtdiags = np.sqrt(np.diag(normcovmat))
     totsqrtdiags = np.sqrt(np.diag(totnormcovmat))
     fig_diag, ax2 = plt.subplots(figsize=(20,7))
-    ax2.plot(100*sqrtdiags, '-o', color="darkorange", label="S")
-    ax2.plot(100*expsqrtdiags, '-o', color="purple", label="C")
-    ax2.plot(100*totsqrtdiags, '-o', color="deepskyblue", label="C+S")
+    ax2.plot(100*sqrtdiags, 'o', color="darkorange", label="S")
+    ax2.plot(100*expsqrtdiags, 'o', color="purple", label="C")
+    ax2.plot(100*totsqrtdiags, 'o', color="deepskyblue", label="C+S")
     ax2.set_ylabel("% of data", fontsize=20)
-    ax2.set_title(f"Diagonal elements of covariance matrix",
+    ax2.set_title(f"Diagonal elements of covariance matrix, {descrip}",
                   fontsize=28)
-    ax2.set_ylim([0, 35])
-    ticklocs, ticklabels, startlocs = matrix_plot_labels(fp1_table)
+    ax2.set_ylim([0, 120])
+    ticklocs, ticklabels, startlocs = matrix_plot_labels(proton)
     plt.xticks(ticklocs, ticklabels, rotation=30, ha="right", fontsize=15)
     # Shift startlocs elements 0.5 to left so lines are between indexes
     startlocs_lines = [x - 0.5 for x in startlocs]
@@ -128,46 +96,109 @@ def covmat_plots(label, fp1_table, fp2_table, fp1_covmat):
     ax2.margins(x=0, y=0)
     ax2.legend(fontsize=15)
     plt.savefig(f"../../plots/covmats/diag_covmat_{label}.png")
+    return fig_diag
 
-    return fig_th, fig_exp, fig_tot, fig_diag
+def make_covmat(label, proton, nuclear, expcov):
+    
+    expcovmat = expcov.values
+
+    proton_theory = proton["theory_central"]
+    D = nuclear["data_central"]
+    nuclear_theory = nuclear["theory_central"]
+    
+    
+    # Calculating errors on T_fp1 by taking standard deviation
+    proton_reps = proton.loc[:, proton.columns.str.contains("rep")]
+    nuclear_reps = nuclear.loc[:, nuclear.columns.str.contains("rep")]
+    nprotonrep = len(proton_reps.values.T)
+    nnuclearrep = len(nuclear_reps.values.T)
+
+    proton_repeat = np.tile(proton_theory.values, (nnuclearrep,1)).T
+    deltas = nuclear_reps.values - proton_repeat
+
+    nuclear_repeat = np.tile(nuclear_theory.values, (nnuclearrep,1)).T
+    deltasprime =  nuclear_reps.values - nuclear_repeat
+
+    shift = nuclear_theory - proton_theory
+    err = np.std(nuclear_reps - proton_repeat, axis=1)
+  
+    fig, ax = plt.subplots(figsize=(20,7))
+    ax.plot(100*(shift/D).values, color="k")
+    ax.plot(100*((shift-err)/D).values, color="k", alpha=0.5)
+    ax.plot(100*((shift+err)/D).values, color="k", alpha=0.5)
+    ax.set_ylabel("% of data", fontsize=20)
+    ax.set_title("Shift", fontsize=25)
+    ticklocs, ticklabels, startlocs = matrix_plot_labels(proton)
+    plt.xticks(ticklocs, ticklabels, rotation=30, ha="right", fontsize=15)
+    # Shift startlocs elements 0.5 to left so lines are between indexes
+    startlocs_lines = [x - 0.5 for x in startlocs]
+    ymin, ymax = ax.get_ylim()
+    xmin, xmax = ax.get_xlim()
+    plt.yticks(fontsize=16)
+    ax.vlines(startlocs_lines, ymin, ymax, linestyles="dashed")
+    ax.margins(x=0, y=0)
+    ax.legend(fontsize=15)
+  #  plt.savefig(f"../../plots/covmats/diag_covmat_{label}.png")
+    
+    covmat = (1/nnuclearrep) * deltas@deltas.T
+    covmat_df = pd.DataFrame(covmat, index=proton.index, columns=proton.index)
+    covmat_df.to_csv(f"covmatrix_{label}.csv")
+    covmatprime = (1/nnuclearrep) * deltasprime@deltasprime.T
+    covmatprime_df = pd.DataFrame(covmatprime, index=proton.index, columns=proton.index)
+    covmatprime_df.to_csv(f"covmatrix_shift_{label}.csv")
+    
+    normcovmat = covmat/np.outer(D.values, D.values)
+    normcovmatshift = covmatprime/np.outer(D.values, D.values)
+    normexpcovmat = expcovmat/np.outer(D.values, D.values)
+    expsqrtdiags = np.sqrt(np.diag(normexpcovmat))
+    totcovmat = covmat + expcovmat
+    totcovmatshift = covmatprime + expcovmat
+    totnormcovmat = totcovmat/np.outer(D.values, D.values)
+    totnormcovmatshift = totcovmatshift/np.outer(D.values, D.values)
+    
+    fig_th = corrmat_plot(covmat, proton, "Theory", label)
+    fig_exp = corrmat_plot(expcovmat, proton, "Experiment", label)
+    fig_tot = corrmat_plot(totcovmat, proton, "Total", label)
+    fig_shiftth = corrmat_plot(covmatprime, proton, "Theory, shifted", label)
+    fig_shifttot = corrmat_plot(totcovmatshift, proton, "Total, shifted", label)
+    
+    diag_element_plot(normcovmat, totnormcovmat, expsqrtdiags, "deweighted", label)
+    diag_element_plot(normcovmatshift, totnormcovmatshift, expsqrtdiags, "shifted", label)
+    
+    return fig_th, fig_exp, fig_tot, fig_shiftth, fig_shifttot
 
 # Loading observables
 
 chorus = pd.read_table(
-    "../observables/CHORUS_nuc/output/tables/group_dataset_inputs_by_experiment0_group_result_table.csv",
+    "../observables/CHORUS_nuc/output/tables/group_result_table.csv",
     dtype={"user_id": float},
     index_col=[0,1,2]
 )
 
 nutev = pd.read_table(
-    "../observables/NUTEV_nuc/output/tables/group_dataset_inputs_by_experiment0_group_result_table.csv",
+    "../observables/NUTEV_nuc/output/tables/group_result_table.csv",
     dtype={"user_id": float},
     index_col=[0,1,2]
 )
 
 e605 = pd.read_table(
-    "../observables/E605_nuc/output/tables/group_dataset_inputs_by_experiment0_group_result_table.csv",
+    "../observables/E605_nuc/output/tables/group_result_table.csv",
     dtype={"user_id": float},
     index_col=[0,1,2]
 )
 
-chorus_proton = pd.read_table(
-    "../observables/proton/output/tables/group_dataset_inputs_by_experiment0_group_result_table.csv",
+proton = pd.read_table(
+    "../observables/proton/output/tables/group_result_table.csv",
     dtype={"user_id": float},
     index_col=[0,1,2]
 )
 
-nutev_proton = pd.read_table(
-    "../observables/proton/output/tables/group_dataset_inputs_by_experiment1_group_result_table.csv",
+expcov = pd.read_table(
+    "../observables/proton/output/tables/groups_covmat.csv",
     dtype={"user_id": float},
-    index_col=[0,1,2]
+    index_col=[0,1,2], header=[0,1,2]
 )
 
-e605_proton = pd.read_table(
-    "../observables/proton/output/tables/group_dataset_inputs_by_experiment2_group_result_table.csv",
-    dtype={"user_id": float},
-    index_col=[0,1,2]
-)
-
-proton =  pd.concat([chorus_proton,nutev_proton,e605_proton])
 nuclear = pd.concat([chorus,nutev,e605])
+
+make_covmat("nuclear", proton, nuclear, expcov)
